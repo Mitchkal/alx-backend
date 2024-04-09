769837ee533d90ca55aea462620623d3e3848750
Mitchkal <mitchellkalenda@gmail.com>
2024-04-09 11:19:45 -0400

    fixed template name

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -5,15 +5,15 @@ Flask app setup
 from flask import Flask, render_template
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 
-@app.route('/')
+@app.route('/', methods=['GET'], strict_slashes=False)
 def index() -> str:
     """
     index function to render template
     """
-    return render_template('index.html')
+    return render_template('0-index.html')
 
 
 if __name__ == '__main__':
