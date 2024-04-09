11c679cb4dfca0d9bfe5d14b32643d128cca6b15
Mitchkal <mitchellkalenda@gmail.com>
2024-04-09 12:51:36 -0400

    renamed file

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -3,9 +3,24 @@
 Flask app setup
 """
 from flask import Flask, render_template
+from flask_babel import Babel
 
 
 app = Flask(__name__, template_folder='templates')
+babel = Babel(app)
+
+
+class Config:
+    """
+    babel configuration
+    for languages
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
