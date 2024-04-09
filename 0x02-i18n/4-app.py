c4d2f16a6918f373c7e5c2bc00745bfc777f6cdc
Mitchkal <mitchellkalenda@gmail.com>
2024-04-09 19:46:01 -0400

    added location detection for language update

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""
+Flask app setup
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+app = Flask(__name__, template_folder='templates')
+babel = Babel(app)
+
+
+class Config(object):
+    """
+    babel configuration
+    for languages
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index() -> str:
+    """
+    index function to render template
+    """
+    return render_template('4-index.html')
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    determines the bestbmatch in supported languages
+    """
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == '__main__':
+    """
+    the main
+    """
+    app.run(debug=True)
